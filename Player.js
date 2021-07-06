2f248d189411282564589da90cc7dd5ee5a002b1
bharaghaveshricg <85169355+bharaghaveshricg@users.noreply.github.com>
2021-07-06 17:08:03 +0530

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,19 +1,18 @@
 class Player{
-
-    constructor(x,y){
-        this.x=x;
-        this.y=y;
-        this.spt=createSprite(this.x,this.y,50,50);
-        this.spt.shapeColor="yellow"
+    constructor(x, y)
+    {
+        this.x = x;
+        this.y = y;
+        this.spt = createSprite(this.x, this.y, 50, 50);
+        this.spt.shapeColor = "orange";
         this.spt.addAnimation("player",playerAnimation);
-        this.spt.scale=0.07;
-    }
-        move(xdir,ydir)
-        {
-        this.spt.x+=xdir*grid;
-        this.spt.y+=ydir*grid;
+        this.spt.scale=0.07;  
 
-        }
-            
     }
 
+    move(xdir, ydir)
+    {
+        this.spt.x += xdir*grid;
+        this.spt.y += ydir*grid;
+    }
+}
